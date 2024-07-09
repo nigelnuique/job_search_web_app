395817872fd265480d54607bb205f23de41a7ab7
Nigel Nuique <67406862+nigelnuique@users.noreply.github.com>
2024-07-09 17:02:26 +1000

    Bind Flask app to PORT environment variable

--- app.py
+++ app.py
@@ -131,4 +131,5 @@ def get_all_jobs_in_category(category):
     return job_urls
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
